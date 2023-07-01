3e4771274c26a145c258eef204daf33cd833ea18
Maiane Gabriele | UI/UX Designer <59965402+sheisacreative@users.noreply.github.com>
2023-07-01 13:26:53 -0300

    Colors generating correctly

--- app/components/Colors.tsx
+++ app/components/Colors.tsx
@@ -1,44 +1,47 @@
+"use client";
 import React from "react";
 import styles from "./Colors.module.css";
 import { useGlobalContext } from "../context";
+import Values from "values.js";
 
 const Colors = () => {
   const { selectedColor } = useGlobalContext();
 
+  const color = new Values(selectedColor);
+  const tints = color.tints(10).slice(0, 9).reverse();
+  const shades = color.shades(10).slice(0, 9);
+
+  const iterateOverColors = (colorArray: Array<Values>) => {
+    return colorArray.map((color, index) => {
+      return (
+        <div
+          className={styles.singleColor}
+          key={index}
+          style={{ backgroundColor: `#${color.hex}` }}
+        >
+          <div className={styles.colorHover}>
+            <p className={`font-sm ${styles.colorName}`}>{`#${color.hex}`}</p>
+            <p className={styles.copyLink}>Copy</p>
+          </div>
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="container">
       <div className={styles.titleContainer}>
-        <h2 className={`title-s`}>shades</h2>
-        <div className={styles.line} />
         <h2 className={`title-s`}>tints</h2>
+        <div className={styles.line} />
+        <h2 className={`title-s`}>shades</h2>
       </div>
       <div className={styles.colorsContainer}>
-        <div className={styles.shades}>
-          <div className={styles.singleColor}>
-            {/* <p className={`font-sm ${styles.colorName}`}>#123456</p>
-            <p className={styles.copyLink}>Copy</p> */}
-          </div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-        </div>
-        <div className={styles.selectedColor}></div>
-        <div className={styles.tints}>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-          <div className={styles.singleColor}></div>
-        </div>
+        <div className={styles.tints}>{iterateOverColors(tints)}</div>
+        <div
+          className={styles.selectedColor}
+          style={{ backgroundColor: selectedColor }}
+        ></div>
+        <div className={styles.shades}>{iterateOverColors(shades)}</div>
       </div>
     </div>
   );
