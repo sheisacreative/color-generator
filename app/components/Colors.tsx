e456981a68a2ddd614f0a0400dcdc56195ade941
Maiane Gabriele | UI/UX Designer <59965402+sheisacreative@users.noreply.github.com>
2023-07-01 15:47:43 -0300

    Single color moved to its own component

--- app/components/Colors.tsx
+++ app/components/Colors.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import styles from "./Colors.module.css";
 import { useGlobalContext } from "../context";
 import Values from "values.js";
+import SingleColor from "./SingleColor";
 
 const Colors = () => {
   const { selectedColor } = useGlobalContext();
@@ -10,23 +11,6 @@ const Colors = () => {
   const color = new Values(selectedColor);
   const tints = color.tints(10).slice(0, 9).reverse();
   const shades = color.shades(10).slice(0, 9);
-
-  const iterateOverColors = (colorArray: Array<Values>) => {
-    return colorArray.map((color, index) => {
-      return (
-        <div
-          className={styles.singleColor}
-          key={index}
-          style={{ backgroundColor: `#${color.hex}` }}
-        >
-          <div className={styles.colorHover}>
-            <p className={`font-sm ${styles.colorName}`}>{`#${color.hex}`}</p>
-            <p className={styles.copyLink}>Copy</p>
-          </div>
-        </div>
-      );
-    });
-  };
 
   return (
     <div className="container">
@@ -36,12 +20,20 @@ const Colors = () => {
         <h2 className={`title-s`}>shades</h2>
       </div>
       <div className={styles.colorsContainer}>
-        <div className={styles.tints}>{iterateOverColors(tints)}</div>
+        <div className={styles.tints}>
+          {tints.map((tint, index) => {
+            return <SingleColor key={index} color={tint.hex} />;
+          })}
+        </div>
         <div
           className={styles.selectedColor}
           style={{ backgroundColor: selectedColor }}
         ></div>
-        <div className={styles.shades}>{iterateOverColors(shades)}</div>
+        <div className={styles.shades}>
+          {shades.map((shade, index) => {
+            return <SingleColor key={index} color={shade.hex} />;
+          })}
+        </div>
       </div>
     </div>
   );
---
+++ app/components/SingleColor.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./SingleColor.module.css";
+
+type Color = {
+  color: string;
+};
+
+const SingleColor = ({ color }: Color) => {
+  return (
+    <div
+      className={styles.singleColor}
+      style={{ backgroundColor: `#${color}` }}
+    >
+      <div className={styles.colorHover}>
+        <p className={`font-sm ${styles.colorName}`}>{`#${color}`}</p>
+        <p className={styles.copyLink}>Copy</p>
+      </div>
+    </div>
+  );
+};
+
+export default SingleColor;
